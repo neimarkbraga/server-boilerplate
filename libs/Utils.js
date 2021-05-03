48b7ac0277f8350115cb78832b822b2cdf43aeb5
Makoy <neimarkbraga@gmail.com>
2021-05-03 18:19:55 +0800

    added getRandomInt util

--- libs/Utils.js
+++ libs/Utils.js
@@ -1,5 +1,9 @@
 class Utils {
-
+  static getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
 
 module.exports = Utils;
